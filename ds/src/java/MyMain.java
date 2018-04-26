deabece26e052f78561ae6b022de4e03d1a330a0
Manikkumar1988 <manikkumar@gmail.com>
2018-04-26 07:13:10 +0530

    array list implementation kick-off

---
+++ ds/src/java/MyMain.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+/**
+ * Created by mani on 25/04/18.
+ */
+public class MyMain {
+    public static void main(String[] args) {
+        System.out.print("a");
+
+        ArrayList<String> testList = new ArrayList<>();
+        System.out.print(testList.size());
+    }
+}
---
+++ ds/test/java/MyArrayListTest.java
@@ -0,0 +1,44 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by mani on 25/04/18.
+ */
+public class MyArrayListTest {
+
+    @Test
+    public void Size_InitializedWithZero_ReturnsZero() {
+        MyArrayList<String> myArrayList = new MyArrayList<>();
+        assertEquals(myArrayList.size(),0);
+    }
+
+    /*@Test
+    public void Size_InitializedWithMaxValue_ReturnsMaxValue() {
+        MyArrayList<String> myArrayList = new MyArrayList<>(Integer.MAX_VALUE);
+        assertEquals(myArrayList.size(),Integer.MAX_VALUE);
+    }*/
+
+    @Test
+    public void Size_InitializedWithOne_ReturnsOne() {
+        MyArrayList<String> myArrayList = new MyArrayList<>(1);
+        assertEquals(myArrayList.size(),1);
+    }
+
+    @Test(expected = Exception.class)
+    public void Size_InitializedWithMinusOne_ThrowsException() {
+        MyArrayList<String> myArrayList = new MyArrayList<>(-1);
+    }
+
+    @Test
+    public void isEmpty_EmptyArray_True() {
+        MyArrayList<String> myArrayList = new MyArrayList<>();
+        assertEquals(myArrayList.isEmpty(),true);
+    }
+
+    @Test
+    public void isEmpty_NonEmptyArray_False() {
+        MyArrayList<String> myArrayList = new MyArrayList<>(10);
+        assertEquals(myArrayList.isEmpty(),false);
+    }
+}
