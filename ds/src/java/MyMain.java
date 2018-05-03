271a7c5de3fafab1b9b9a2369252210d798a7c87
Manikkumar1988 <manikkumar@gmail.com>
2018-05-03 06:32:30 +0530

    Testing the code coverage

--- ds/src/java/MyMain.java
+++ ds/src/java/MyMain.java
@@ -7,7 +7,7 @@ import java.util.stream.Collectors;
  */
 public class MyMain {
     public static void main(String[] args) {
-        System.out.print("a");
+        /*System.out.print("a");
 
         ArrayList<String> testList = new ArrayList<>(10);
         System.out.print(testList.size());
@@ -15,23 +15,23 @@ public class MyMain {
         for(int i=0;i<500;i++)
             testList.add(String.valueOf(i));
 
-        /*List<String> result = testList.stream()
+        List<String> result = testList.stream()
                 .map(x -> multiplyByTwo(x))
                 .filter(x -> customFilter(x))
                 .collect(Collectors.toList());
 
-        System.out.print(result);*/
+        System.out.print(result);
 
 
 
-        System.out.print(testList.indexOf("1"));
+        System.out.print(testList.indexOf("1"));*/
     }
 
-    private static String multiplyByTwo(String x) {
+    /*private static String multiplyByTwo(String x) {
         return String.valueOf(Integer.valueOf(x)*3);
     }
 
     private static boolean customFilter(String x) {
         return (Integer.valueOf(x)%2==0);
-    }
+    }*/
 }
